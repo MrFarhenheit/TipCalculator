e347d8dfa2b8562f9cffa8f70d8ef1537402eb26
MrFarhenheit <zclaypoole1@gmail.com>
2023-12-12 17:56:19 -0500

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Zac Claypoole on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
